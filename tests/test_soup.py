e7dee599b103184776d232118a7d0bdeb056136e
Sean Naderi <seannaderi@dyn1211-200.wlan.ic.ac.uk>
2017-02-16 14:32:29 +0000

    added the structure for unit tests

---
+++ tests/test_soup.py
@@ -0,0 +1,25 @@
+import unittest
+
+class test_soup(unittest.TestCase):
+    #set up for all of the soup test functions
+    def setUp(self):
+
+    #tear down for all of the soup test functions
+    def tearDown(self):
+
+    def test_1(self):
+        pass
+    
+    def test_2(self):
+        pass
+
+
+"""
+Using the following asserts:
+
+1) self.assertEqual(1,2)
+2) self.assertTrue(1)
+3) self.assertFalse(3)
+
+
+"""
