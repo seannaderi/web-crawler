ac15acd5f05c2fc2be88a95d7bf13bff1dcc4a07
Sean Naderi <seannaderi@dyn1211-200.wlan.ic.ac.uk>
2017-02-19 16:28:57 +0000

    polished up the testing for html_page class

--- tests/html_page_tests.py
+++ tests/html_page_tests.py
@@ -3,8 +3,8 @@ import os
 from crawler import html_page
 
 class html_page_tests(unittest.TestCase):
-
-    #set up for all of the soup test functions
+    
+    # Makes html_page class with four_links.html for testing
     def setUp(self):
         current_dir = os.path.dirname(__file__)
         filename = os.path.join(current_dir, 'mocks/four_links.html') 
@@ -12,33 +12,28 @@ class html_page_tests(unittest.TestCase):
             self.html_string = myfile.read().replace('\n', '')
         self.test_html_page = html_page.html_page(self.html_string)
 
-    #tear down for all of the soup test functions
     def tearDown(self):
         return
+    
+    def test_html_class_is_created(self):
+        self.assertTrue(self.test_html_page is not None)
 
     def test_html_class_parses_links(self):
         self.assertTrue('/third_link.html' in self.test_html_page.html_links)
     
-    def test_html_class_parses_static_assets(self):
+    def test_html_class_parses_scripts(self):
         self.assertTrue('/first_link.js' in self.test_html_page.static_assets['scripts'])
+       
+    def test_html_class_parses_css(self):
         self.assertTrue('/second_link.css' in self.test_html_page.static_assets['css'])
-        self.assertTrue('/fourth_link.jpg' in self.test_html_page.static_assets['img'])
-#creates the test suite, instantiating each instance to call a different
-#test method
-#def suite():
-#    tests = ['test_3', 'test_2']
-#    return unittest.TestSuite(map(test_soup, tests))
 
+    def test_html_class_parses_imgs(self):
+        self.assertTrue('/fourth_link.jpg' in self.test_html_page.static_assets['img'])
+
+    def test_html_class_correctly_splits_static_assets(self):
+        self.assertFalse('/fourth_link.jpg' in self.test_html_page.static_assets['scripts'])
+        self.assertFalse('/second_link.css' in self.test_html_page.static_assets['img'])
+        self.assertFalse('/first_link.js' in self.test_html_page.static_assets['css'])
+ 
 if __name__ == '__main__':
     unittest.main()
-	
-
-"""
-Using the following asserts:
-
-1) self.assertEqual(1,2)
-2) self.assertTrue(1)
-3) self.assertFalse(3)
-
-
-"""
